5b2c09245dfe6fb6168fd094e6b507b026feaee4
ali <erali.jurakulov@mail.ru>
2026-04-30 20:45:28 +0900

    feat: integration Header and Footer via Layouts

---
+++ pages/property/detail.tsx
@@ -0,0 +1,9 @@
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import { Container, Stack } from "@mui/material";
+
+const PropertyDetail = () => {
+  console.log("PROPERTYDETAIL COMPONENT - PAGES ROUTER");
+  return <Container>PROPERTY DETAIL</Container>;
+};
+
+export default withLayoutBasic(PropertyDetail);
--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -1,6 +1,11 @@
-const PropertyList = () => {
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import Container from "@mui/material/Container/Container";
+import Stack from "@mui/material/Stack/Stack";
+import { NextPage } from "next";
+
+const PropertyList: NextPage = () => {
   console.log("PROPERTYLIST COMPONENT - PAGES ROUTER");
-  return <div>PROPERTY LIST</div>;
+  return <Container>PROPERTY LIST</Container>;
 };
 
-export default PropertyList;
+export default withLayoutBasic(PropertyList);
